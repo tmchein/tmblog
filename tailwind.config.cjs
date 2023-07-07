7afec192f77b13c959ff521c4e5a4c7ee70945ff
Juan rojas <davidr2998@gmail.com>
2023-07-07 15:57:33 -0500

    feat ✨: Added dark mode
    add dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnight: "#161616",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
